8ae211c2c90da856dbcac414601e03da4cf3b880
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-12 21:58:31 -0600

    feat(ui): add LanguageSelector to Navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -8,6 +8,7 @@ import NavItem from "./NavItem";
 import MobileNav from "./MobileNav";
 import Logo from "../logo";
 import { Icons } from "@/components/ui/icons";
+import LanguageSelector from "../languageSelector/LanguageSelector";
 
 interface Props {
   items?: MainNavItem[];
@@ -18,23 +19,29 @@ const Navbar = ({ items, children }: Props) => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   return (
-    <header className="flex justify-between md:justify-start gap-6 md:gap-10">
-      <Link href={"/"}>
-        <Logo />
-      </Link>
+    <header className="flex items-center justify-between">
+      <div className="flex items-center gap-6 md:gap-10">
+        <Link href={"/"}>
+          <Logo />
+        </Link>
 
-      {items?.length ? (
-        <nav className="hidden gap-6 md:flex">
-          {items?.map((item) => (
-            <NavItem
-              key={item.title}
-              title={item.title}
-              href={item.href}
-              disabled={item.disabled}
-            />
-          ))}
-        </nav>
-      ) : null}
+        {items?.length ? (
+          <nav className="hidden gap-6 md:flex">
+            {items?.map((item) => (
+              <NavItem
+                key={item.title}
+                title={item.title}
+                href={item.href}
+                disabled={item.disabled}
+              />
+            ))}
+          </nav>
+        ) : null}
+      </div>
+
+      <div className="hidden md:block">
+        <LanguageSelector />
+      </div>
 
       <button
         className="flex items-center space-x-2 md:hidden"
