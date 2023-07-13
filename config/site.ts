93364e97234b13d0fcbbc0fe52b5eab30d4696f3
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-12 20:15:34 -0600

    config(ui): create site config data

---
+++ config/site.ts
@@ -0,0 +1,22 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  mainNav: [
+    {
+      title: "Movies",
+      href: "/movies",
+    },
+    {
+      title: "TV Shows",
+      href: "/pricing",
+    },
+    {
+      title: "People",
+      href: "/people",
+    },
+    {
+      title: "More",
+      href: "/more",
+    },
+  ],
+};
