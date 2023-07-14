a259fba139a9e72b02a8d32a160161dc8a21f048
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-13 19:14:18 -0600

    feat(ui): add Hero component to HomePage

--- app/[locale]/(application)/page.tsx
+++ app/[locale]/(application)/page.tsx
@@ -1,8 +1,15 @@
+import { Hero } from "@/app/components/hero/Hero";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <Hero
+        title="Welcome"
+        subtitle="Millions of movies, TV shows and people to discover. Explore now."
+      />
+
+      <section className="container py-4"></section>
+    </>
   );
 };
 
