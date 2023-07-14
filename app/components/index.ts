2477f3561b5789363353b69732c2b8a931479f88
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-14 01:24:37 -0600

    update(export): add new component to index.ts components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Carrousel } from "./movie";
+export { default as Footer } from "./footer";
 export { default as Hero } from "./hero";
 export { default as LanguageSelector } from "./languageSelector";
-export { default as Logo } from "./logo";
 export { default as Navbar } from "./navbar";
