78a43e3a23f2d72a69d7b1328d68435d1cd8f893
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-15 16:14:47 -0600

    feat(export): add Circular component to export file

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Carrousel } from "./movie";
+export { default as CircularLoader } from "./circularLoader";
 export { default as Footer } from "./footer";
 export { default as Hero } from "./hero";
 export { default as LanguageSelector } from "./languageSelector";
