f43baf99f59defaf09c3045c49de3779e3d3f988
Oscar Estrada <arturostrada2020@gmail.com>
2023-07-17 20:59:38 -0600

    feat(ui): add new icons

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,8 +1,21 @@
-import { AlignRight, SunMedium, Moon, Laptop } from "lucide-react";
+import {
+  AlignRight,
+  SunMedium,
+  Moon,
+  Laptop,
+  Facebook,
+  Twitter,
+  Instagram,
+  Link2,
+} from "lucide-react";
 
 export const Icons = {
   menuIcon: AlignRight,
   sun: SunMedium,
   moon: Moon,
   laptop: Laptop,
+  facebook: Facebook,
+  twitter: Twitter,
+  instagram: Instagram,
+  externalLink: Link2,
 };
